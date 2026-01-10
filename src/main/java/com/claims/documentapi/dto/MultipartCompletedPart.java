d34fb4408852c5fbbbddffd9e854332c9565c7b5
mohamedelsheikha <mohamed.elsheikha@gmail.com>
2026-01-10 18:00:39 -0500

    feat: upload large files using multipart/presigned

--- src/main/java/com/claims/documentapi/dto/MultipartCompletedPart.java
+++ src/main/java/com/claims/documentapi/dto/MultipartCompletedPart.java
@@ -1,9 +1,11 @@
 package com.claims.documentapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class MultipartCompletedPart {
     private Integer partNumber;
+    @JsonProperty("eTag")
     private String eTag;
 }
